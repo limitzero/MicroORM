6e1651a1d3dce35fa3aff7a316773d4110f4abef
Michael Calloway <Michael.Calloway@e-hps.com>
2014-11-12 15:55:44 -0600

    adding default logging

---
+++ src/Logging/ConsoleLogger.cs
@@ -0,0 +1,185 @@
+﻿using System;
+
+namespace MicroORM.Logging
+{
+    public class ConsoleLogger : ILogger
+    {
+        public bool IsDebugEnabled { get; private set; }
+        public bool IsInfoEnabled { get; private set; }
+        public bool IsErrorEnabled { get; private set; }
+        public bool IsWarnEnabled { get; private set; }
+        public bool IsFatalEnabled { get; private set; }
+
+        public ConsoleLogger()
+        {
+            IsDebugEnabled = true;
+            IsErrorEnabled = true;
+            IsFatalEnabled = true;
+            IsWarnEnabled = true;
+        }
+
+        public void Debug(string message)
+        {
+            Log("DEBUG", null, null, message);
+        }
+
+        public void Debug(string message, Exception exception)
+        {
+            Log("DEBUG", exception, null, message);
+        }
+
+        public void DebugFormat(string format, params object[] args)
+        {
+            Log("DEBUG", null, null, format, args);
+        }
+
+        public void DebugFormat(Exception exception, string format, params object[] args)
+        {
+            Log("DEBUG", exception, null, format, args);
+        }
+
+        public void DebugFormat(IFormatProvider formatProvider, string format, params object[] args)
+        {
+            Log("DEBUG", null, formatProvider, format, args);
+        }
+
+        public void DebugFormat(Exception exception, IFormatProvider formatProvider, string format, params object[] args)
+        {
+            Log("DEBUG", exception, formatProvider, format, args);
+        }
+
+        public void Info(string message)
+        {
+            Log("INFO", null, null, message);
+        }
+
+        public void Info(string message, Exception exception)
+        {
+            Log("INFO", exception, null, message);
+        }
+
+        public void InfoFormat(string format, params object[] args)
+        {
+            Log("INFO", null, null, format, args);
+        }
+
+        public void InfoFormat(Exception exception, string format, params object[] args)
+        {
+            Log("INFO", exception, null, format, args);
+        }
+
+        public void InfoFormat(IFormatProvider formatProvider, string format, params object[] args)
+        {
+            Log("INFO", null, formatProvider, format, args);
+        }
+
+        public void InfoFormat(Exception exception, IFormatProvider formatProvider, string format, params object[] args)
+        {
+            Log("INFO", exception, formatProvider, format, args);
+        }
+
+        public void Error(string message)
+        {
+            Log("ERROR", null, null, message);
+        }
+
+        public void Error(string message, Exception exception)
+        {
+            Log("ERROR", exception, null, message);
+        }
+
+        public void ErrorFormat(string format, params object[] args)
+        {
+            Log("ERROR", null, null, format, args);
+        }
+
+        public void ErrorFormat(Exception exception, string format, params object[] args)
+        {
+            Log("ERROR", exception, null, format, args);
+        }
+
+        public void ErrorFormat(IFormatProvider formatProvider, string format, params object[] args)
+        {
+            Log("ERROR", null, formatProvider, format, args);
+        }
+
+        public void ErrorFormat(Exception exception, IFormatProvider formatProvider, string format, params object[] args)
+        {
+            Log("ERROR", exception, formatProvider, format, args);
+        }
+
+        public void Warn(string message)
+        {
+            Log("WARN", null, null, message);
+        }
+
+        public void Warn(string message, Exception exception)
+        {
+            Log("WARN", exception, null, message);
+        }
+
+        public void WarnFormat(string format, params object[] args)
+        {
+            Log("WARN", null, null, format, args);
+        }
+
+        public void WarnFormat(Exception exception, string format, params object[] args)
+        {
+            Log("WARN", exception, null, format, args);
+        }
+
+        public void WarnFormat(IFormatProvider formatProvider, string format, params object[] args)
+        {
+            Log("WARN", null, formatProvider, format, args);
+        }
+
+        public void WarnFormat(Exception exception, IFormatProvider formatProvider, string format, params object[] args)
+        {
+            Log("WARN", exception, formatProvider, format, args);
+        }
+
+        public void Fatal(string message)
+        {
+            Log("FATAL", null, null, message);
+        }
+
+        public void Fatal(string message, Exception exception)
+        {
+            Log("FATAL", exception, null, message);
+        }
+
+        public void FatalFormat(string format, params object[] args)
+        {
+            Log("FATAL", null, null, format, args);
+        }
+
+        public void FatalFormat(Exception exception, string format, params object[] args)
+        {
+            Log("FATAL", exception, null, format, args);
+        }
+
+        public void FatalFormat(IFormatProvider formatProvider, string format, params object[] args)
+        {
+            Log("FATAL", null, formatProvider, format, args);
+        }
+
+        public void FatalFormat(Exception exception, IFormatProvider formatProvider, string format, params object[] args)
+        {
+            Log("FATAL", exception, formatProvider, format, args);
+        }
+
+        private void Log(string level, Exception exception, IFormatProvider formatProvider,
+            string format,
+            params object[] args)
+        {
+            var message = string.Format("MicroORM - {0} - {1} - {2}", System.DateTime.Now,
+                level,
+                string.Format(formatProvider, format, args));
+
+            if (exception != null)
+                message += string.Format(" - {0}", exception);
+
+            System.Console.WriteLine(message);
+        }
+    }
+}
---
+++ src/Logging/ILogger.cs
@@ -0,0 +1,234 @@
+﻿using System;
+
+namespace MicroORM.Logging
+{
+    public interface ILogger
+    {
+        bool IsDebugEnabled { get; }
+        bool IsInfoEnabled { get; }
+        bool IsErrorEnabled { get; }
+        bool IsWarnEnabled { get; }
+        bool IsFatalEnabled { get; }
+
+        /// <summary>
+        ///   Logs a debug message.
+        /// </summary>
+        void Debug(string message);
+
+        /// <summary>
+        ///   Logs a debug message.
+        /// </summary>
+        /// <param name="exception"> The exception to log </param>
+        /// <param name="message"> The message to log </param>
+        void Debug(string message, Exception exception);
+
+        /// <summary>
+        ///   Logs a debug message.
+        /// </summary>
+        /// <param name="format"> Format string for the message to log </param>
+        /// <param name="args"> Format arguments for the message to log </param>
+        void DebugFormat(string format, params object[] args);
+
+        /// <summary>
+        ///   Logs a debug message.
+        /// </summary>
+        /// <param name="exception"> The exception to log </param>
+        /// <param name="format"> Format string for the message to log </param>
+        /// <param name="args"> Format arguments for the message to log </param>
+        void DebugFormat(Exception exception, string format, params object[] args);
+
+        /// <summary>
+        ///   Logs a debug message.
+        /// </summary>
+        /// <param name="formatProvider"> The format provider to use </param>
+        /// <param name="format"> Format string for the message to log </param>
+        /// <param name="args"> Format arguments for the message to log </param>
+        void DebugFormat(IFormatProvider formatProvider, string format, params object[] args);
+
+        /// <summary>
+        ///   Logs a debug message.
+        /// </summary>
+        /// <param name="exception"> The exception to log </param>
+        /// <param name="formatProvider"> The format provider to use </param>
+        /// <param name="format"> Format string for the message to log </param>
+        /// <param name="args"> Format arguments for the message to log </param>
+        void DebugFormat(Exception exception, IFormatProvider formatProvider, string format, params object[] args);
+
+        /// <summary>
+        ///   Logs a info message.
+        /// </summary>
+        void Info(string message);
+
+        /// <summary>
+        ///   Logs a info message.
+        /// </summary>
+        /// <param name="exception"> The exception to log </param>
+        /// <param name="message"> The message to log </param>
+        void Info(string message, Exception exception);
+
+        /// <summary>
+        ///   Logs a info message.
+        /// </summary>
+        /// <param name="format"> Format string for the message to log </param>
+        /// <param name="args"> Format arguments for the message to log </param>
+        void InfoFormat(string format, params object[] args);
+
+        /// <summary>
+        ///   Logs a info message.
+        /// </summary>
+        /// <param name="exception"> The exception to log </param>
+        /// <param name="format"> Format string for the message to log </param>
+        /// <param name="args"> Format arguments for the message to log </param>
+        void InfoFormat(Exception exception, string format, params object[] args);
+
+        /// <summary>
+        ///   Logs a info message.
+        /// </summary>
+        /// <param name="formatProvider"> The format provider to use </param>
+        /// <param name="format"> Format string for the message to log </param>
+        /// <param name="args"> Format arguments for the message to log </param>
+        void InfoFormat(IFormatProvider formatProvider, string format, params object[] args);
+
+        /// <summary>
+        ///   Logs a info message.
+        /// </summary>
+        /// <param name="exception"> The exception to log </param>
+        /// <param name="formatProvider"> The format provider to use </param>
+        /// <param name="format"> Format string for the message to log </param>
+        /// <param name="args"> Format arguments for the message to log </param>
+        void InfoFormat(Exception exception, IFormatProvider formatProvider, string format, params object[] args);
+
+
+        /// <summary>
+        ///   Logs a error message.
+        /// </summary>
+        void Error(string message);
+
+        /// <summary>
+        ///   Logs a error message.
+        /// </summary>
+        /// <param name="exception"> The exception to log </param>
+        /// <param name="message"> The message to log </param>
+        void Error(string message, Exception exception);
+
+        /// <summary>
+        ///   Logs a error message.
+        /// </summary>
+        /// <param name="format"> Format string for the message to log </param>
+        /// <param name="args"> Format arguments for the message to log </param>
+        void ErrorFormat(string format, params object[] args);
+
+        /// <summary>
+        ///   Logs a error message.
+        /// </summary>
+        /// <param name="exception"> The exception to log </param>
+        /// <param name="format"> Format string for the message to log </param>
+        /// <param name="args"> Format arguments for the message to log </param>
+        void ErrorFormat(Exception exception, string format, params object[] args);
+
+        /// <summary>
+        ///   Logs a error message.
+        /// </summary>
+        /// <param name="formatProvider"> The format provider to use </param>
+        /// <param name="format"> Format string for the message to log </param>
+        /// <param name="args"> Format arguments for the message to log </param>
+        void ErrorFormat(IFormatProvider formatProvider, string format, params object[] args);
+
+        /// <summary>
+        ///   Logs a error message.
+        /// </summary>
+        /// <param name="exception"> The exception to log </param>
+        /// <param name="formatProvider"> The format provider to use </param>
+        /// <param name="format"> Format string for the message to log </param>
+        /// <param name="args"> Format arguments for the message to log </param>
+        void ErrorFormat(Exception exception, IFormatProvider formatProvider, string format, params object[] args);
+
+        /// <summary>
+        ///   Logs a warning message.
+        /// </summary>
+        void Warn(string message);
+
+        /// <summary>
+        ///   Logs a warning message.
+        /// </summary>
+        /// <param name="exception"> The exception to log </param>
+        /// <param name="message"> The message to log </param>
+        void Warn(string message, Exception exception);
+
+        /// <summary>
+        ///   Logs a warning message.
+        /// </summary>
+        /// <param name="format"> Format string for the message to log </param>
+        /// <param name="args"> Format arguments for the message to log </param>
+        void WarnFormat(string format, params object[] args);
+
+        /// <summary>
+        ///   Logs a warning message.
+        /// </summary>
+        /// <param name="exception"> The exception to log </param>
+        /// <param name="format"> Format string for the message to log </param>
+        /// <param name="args"> Format arguments for the message to log </param>
+        void WarnFormat(Exception exception, string format, params object[] args);
+
+        /// <summary>
+        ///   Logs a warning message.
+        /// </summary>
+        /// <param name="formatProvider"> The format provider to use </param>
+        /// <param name="format"> Format string for the message to log </param>
+        /// <param name="args"> Format arguments for the message to log </param>
+        void WarnFormat(IFormatProvider formatProvider, string format, params object[] args);
+
+        /// <summary>
+        ///   Logs a warning message.
+        /// </summary>
+        /// <param name="exception"> The exception to log </param>
+        /// <param name="formatProvider"> The format provider to use </param>
+        /// <param name="format"> Format string for the message to log </param>
+        /// <param name="args"> Format arguments for the message to log </param>
+        void WarnFormat(Exception exception, IFormatProvider formatProvider, string format, params object[] args);
+
+        /// <summary>
+        ///   Logs a fatal message.
+        /// </summary>
+        void Fatal(string message);
+
+        /// <summary>
+        ///   Logs a fatal message.
+        /// </summary>
+        /// <param name="exception"> The exception to log </param>
+        /// <param name="message"> The message to log </param>
+        void Fatal(string message, Exception exception);
+
+        /// <summary>
+        ///   Logs a fatal message.
+        /// </summary>
+        /// <param name="format"> Format string for the message to log </param>
+        /// <param name="args"> Format arguments for the message to log </param>
+        void FatalFormat(string format, params object[] args);
+
+        /// <summary>
+        ///   Logs a fatal message.
+        /// </summary>
+        /// <param name="exception"> The exception to log </param>
+        /// <param name="format"> Format string for the message to log </param>
+        /// <param name="args"> Format arguments for the message to log </param>
+        void FatalFormat(Exception exception, string format, params object[] args);
+
+        /// <summary>
+        ///   Logs a fatal message.
+        /// </summary>
+        /// <param name="formatProvider"> The format provider to use </param>
+        /// <param name="format"> Format string for the message to log </param>
+        /// <param name="args"> Format arguments for the message to log </param>
+        void FatalFormat(IFormatProvider formatProvider, string format, params object[] args);
+
+        /// <summary>
+        ///   Logs a fatal message.
+        /// </summary>
+        /// <param name="exception"> The exception to log </param>
+        /// <param name="formatProvider"> The format provider to use </param>
+        /// <param name="format"> Format string for the message to log </param>
+        /// <param name="args"> Format arguments for the message to log </param>
+        void FatalFormat(Exception exception, IFormatProvider formatProvider, string format, params object[] args);
+    }
+}
